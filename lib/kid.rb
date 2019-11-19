2bbfe497d267d08975a9957bc20c0b0a22e81baa
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-19 01:10:15 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,8 @@ require_relative './class_methods_module.rb'
 
 class Kid
   
-  extend FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
+  include FancyDance::ClassMethods
   
   attr_accessor :name
   
