3369f2e08a84a491b6819a000a6bfc7068ca2f51
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-19 01:09:59 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,8 +3,8 @@ require_relative './dance_module.rb'
 
 class Dancer
   
-  extend FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
   attr_accessor :name
 
