e91f11a8a8a645b031fc1762ec4858948294730f
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-19 01:16:58 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  include Dance
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+end
+
+# it 'extends the MetaDancing module' do
+# expect(Dancer).to have_class_dance_methods
